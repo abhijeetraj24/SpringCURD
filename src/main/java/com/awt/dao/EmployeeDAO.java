654b46f59193686d2380a9f10b16321ad09cc985
Lenovo <Lenovo@LAPTOP-40LOLOIM>
2018-08-16 22:01:43 +0530

    TEst

---
+++ src/main/java/com/awt/dao/EmployeeDAO.java
@@ -0,0 +1,19 @@
+package com.awt.dao;
+
+import java.util.List;
+
+import com.jwt.model.Employee;
+
+public interface EmployeeDAO {
+
+	public void addEmployee(Employee employee);
+	 
+    public List<Employee> getAllEmployees();
+ 
+    public void deleteEmployee(Integer employeeId);
+ 
+    public Employee updateEmployee(Employee employee);
+ 
+    public Employee getEmployee(int employeeid);
+    
+}
